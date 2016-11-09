7ef175eae241001142a25595be85b41b81e54a97
Jesse Jutson <jcjutson@gmail.com>
2016-11-09 15:20:54 -0800

    it compiles now

--- FESS-GUI/commoninterfaceselector.cpp
+++ FESS-GUI/commoninterfaceselector.cpp
@@ -4,11 +4,11 @@
 #include "commoninterfaceselector.h"
 #include "ui_commoninterfaceselector.h"
 
-CommonInterfaceSelector::CommonInterfaceSelector(CommonInterfaceManager* interface, QWidget *parent) : QDialog(parent), ui(new Ui::CommonInterfaceSelector)
+CommonInterfaceSelector::CommonInterfaceSelector(CommonInterfaceManager* interfaceManager, QWidget *parent) : QDialog(parent), ui(new Ui::CommonInterfaceSelector)
 {
     ui->setupUi(this);
 
-    interfaceManager = interface;
+    interfaceManager = interfaceManager;
     serialPortTextModified = false;
 
     setUpSignals();
