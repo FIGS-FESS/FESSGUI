609e26a2853ce95ac2fda282b845c97bac5ae6f1
Jesse Jutson <jcjutson@gmail.com>
2016-11-11 17:41:40 -0800

    removed errorstrings.h

--- FESS-GUI/errorstrings.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERRORSTRINGS
-#define ERRORSTRINGS
-
-const QString MAINWINDOW_ERROR = "FlyCAM - Error";
-const QString INTERFACE_DIALOG_ERROR = "FlyCAM - Selection Error";
-
-#endif // ERRORSTRINGS
-
